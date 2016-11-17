075c6b9479a61086db0312b481bb5579ac1add89
alexaudretsch <alexaudretsch@providence.org>
2016-11-17 12:37:31 -0800

    Add view scaffolding

--- Noteworthy/Controllers/HomeController.cs
+++ Noteworthy/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Noteworthy.Models;
+using Noteworthy.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -8,8 +10,19 @@ namespace Noteworthy.Controllers
  //   [Authorize]
     public class HomeController : Controller
     {
+        public ApplicationDbContext Context { get; set; }
+        private readonly User DummyUser = new Models.Entities.User() { Id = 1, DisplayName = "Test User", Email = "test@user.com" };
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Audio()
+        {
+            var records = Context.Notes.OrderBy(n => n.Title).ToList();
+            var viewModel = new AudioDisplayViewModel(records, DummyUser);
             return View();
         }
     }
---
+++ Noteworthy/Models/AudioDisplayViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Noteworthy.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Noteworthy.Models
+{
+    public class AudioDisplayViewModel
+    {
+        public IList<Note> Notes { get; set; }
+
+        public User CurrentUser { get; set; }
+
+        public AudioDisplayViewModel(IList<Note> notes, User user)
+        {
+            Notes = notes;
+            CurrentUser = user;
+        }
+    }
+}
